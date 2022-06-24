b3c5acc14eb9ceb3b05befa09b31495ca0f755f4
Felipe <Felipe@PC-FELIPE>
2022-06-23 21:00:50 -0300

    Adicionar arquivos de projeto.

---
+++ w3Resources/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input: ");
+int value1 = int.Parse(Console.ReadLine());
+int value2 = int.Parse(Console.ReadLine());
+int result = 0;
+if(value1 != value2)
+{
+    result = value1 + value2;   
+}
+else
+{
+    result = 3 * (value1 + value2);
+}
+Console.WriteLine(result);
