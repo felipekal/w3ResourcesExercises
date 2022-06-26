a864fa57fc176b834996827689208833f676ed91
felipekal <felipe.kal@outlook.com>
2022-06-26 09:28:58 -0300

    Exercicio 6: Write a C# Sharp program to remove the character in a given position of a given string. The given position will be in the range 0...string length -1 inclusive.

--- w3Resources/Program.cs
+++ w3Resources/Program.cs
@@ -1,12 +1,8 @@
-﻿string s = Console.ReadLine();
+﻿Console.Write("Sentence: ");
+string s = Console.ReadLine();
+Console.Write("Index: ");
+int n = int.Parse(Console.ReadLine());
 
-string s2 = "if";
-
-if (s.StartsWith("if"))
-{
-    Console.WriteLine(s);
-}
-else
-{
-    Console.WriteLine(s2+ " " + s);
-}
+string str = s.Remove(n, 1);
+Console.WriteLine($"Removed the letter {s[n]} from the original sentence");
+Console.WriteLine($"New word is {str}");
